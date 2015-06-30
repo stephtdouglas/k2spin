eb3063190a89db22d23adbd034f88ef0bf5c3702
stephtdouglas <stephanietdouglas@gmail.com>
2015-06-30 16:27:28 -0400

    module for reading in lightcurves

---
+++ io.py
@@ -0,0 +1,17 @@
+"""Read in lightcurve files."""
+
+import logging
+
+import numpy as np
+import astropy.io.ascii as at
+
+
+def read_lc():
+    """Read in a lightcurve."""
+    pass
+
+
+def read_list():
+    """Read in a list of lightcurve filenames."""
+    pass
+
