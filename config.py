17493a7230c768bcbe8f3d954da228a4fd77e00c
stephtdouglas <stephanietdouglas@gmail.com>
2015-10-13 14:51:41 -0400

    set paths

---
+++ config.py
@@ -0,0 +1,13 @@
+import logging, os
+
+import matplotlib 
+
+logging.basicConfig(level=logging.WARNING)
+
+if os.path.exists("/home/stephanie/Dropbox/")==True:
+    base_path = "/home/stephanie/code/python/k2spin/"
+    logging.warning("k2spin on jaina")
+else:
+    base_path = "/vega/astro/users/sd2706/k2/"
+    logging.warning("Working on Yeti")
+    matplotlib.use("agg")
